c3bcec5088b35e39c324354c84db3561faea58ab
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-07 16:19:46 +0900

    feat: (Address) 주소 엔티티 추가

---
+++ src/main/java/hellojpa/domain/AddressEntity.java
@@ -0,0 +1,31 @@
+package hellojpa.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "address")
+public class AddressEntity {
+    @Id @GeneratedValue
+    private Long id;
+
+    private Address address;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
